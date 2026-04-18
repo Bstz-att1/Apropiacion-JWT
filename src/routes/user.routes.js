6bd0f686df81c38a1c0326ee0733b21f3a6367c6
Btsz-att1 <hfdarioalexander7@gmail.com>
2026-04-18 16:41:55 -0500

    feat: configurar rutas de usuario protegidas con validateToken

---
+++ src/routes/user.routes.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import { 
+    createUser, 
+    deleteUser, 
+    getUserById, 
+    getUsers, 
+    updateUser 
+} from "../controllers/user.controller";
+
+import { validateToken } from "../middlewares/auth.middleware";
+
+const userRouter = Router();
+
+// Aplicar validacion de JWT a todas las rutas de este router
+userRouter.use(validateToken);
+
+// Obtener todos los usuarios
+userRouter.get("/", getUsers);
+
+// Obtener un usuario específico por su ID
+userRouter.get("/:id", getUserById);
+
+// Registrar un nuevo usuario
+userRouter.post("/", createUser);
+
+// Actualizar los datos de un usuario existente
+userRouter.put("/:id", updateUser);
+
+// Eliminar un usuario del sistema
+userRouter.delete("/:id", deleteUser);
+
+export default userRouter;
