dbd25e2a63d325863cdad02bc1795c5d44c0b20c
Bstz-att1 <hfdarioalexander7@gmail.com>
2026-04-23 20:29:29 -0500

    feat(routes): aplicar control de acceso (RBAC) a las rutas de categorías

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -5,22 +5,28 @@ import {
   createCategory,
   updateCategory,
   deleteCategory,
-  getProductsByCategory, // Controlador especial para la relación
+  getProductsByCategory,
 } from "../controllers/category.controller.js";
 
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import { categorySchema } from "../schemas/category.schema.js";
 
+// --- IMPORTAMOS LOS GUARDIAS ---
+import { validateToken } from "../middlewares/auth.middleware.js";
+import { checkPermission } from "../middlewares/permission.middleware.js";
+
 const categoryRouter = Router();
 
-categoryRouter.get("/", getAllCategories);
-categoryRouter.get("/:id", getCategoryById);
-categoryRouter.post("/", validateSchema(categorySchema), createCategory);
-categoryRouter.put("/:id", validateSchema(categorySchema), updateCategory);
-categoryRouter.delete("/:id", deleteCategory);
+categoryRouter.use(validateToken);
+
+categoryRouter.get("/", checkPermission("categories.get"),getAllCategories);
+categoryRouter.get("/:id", checkPermission("categories.get") ,getCategoryById);
+categoryRouter.post("/", checkPermission("categories.create") ,validateSchema(categorySchema), createCategory);
+categoryRouter.put("/:id", checkPermission("categories.update"),validateSchema(categorySchema), updateCategory);
+categoryRouter.delete("/:id", checkPermission("categories.delete") ,deleteCategory);
 
 // Ruta Relacional: Obtener productos por categoría
 // Sigue el estándar REST: /recurso-padre/:id/recurso-hijo
-categoryRouter.get("/:id/products", getProductsByCategory);
+categoryRouter.get("/:id/products", checkPermission("categories.get"),getProductsByCategory);
 
 export default categoryRouter;
